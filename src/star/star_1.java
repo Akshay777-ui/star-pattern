cab02e64ccc78dd893cfdd390a1edb8db9f39fea
user <user@DESKTOP-53NDHPP>
2021-08-01 18:27:23 +0530

    star pattern for your refrence

---
+++ src/star/star_1.java
@@ -0,0 +1,10 @@
+package star;
+
+public class star_1 {
+public static void main(String[] args) {
+	for (int i = 0; i <=4; i++) {
+		System.out.print(" * ");
+		
+	}
+}
+}
---
+++ src/star/star_5.java
@@ -0,0 +1,16 @@
+package star;
+
+public class star_5 {
+public static void main(String[] args) {
+	int star=5;
+	for (int i = 0; i<=5; i++) {
+		for (int j = 0; j <=star; j++) {
+			System.out.print(" * ");
+			
+		}
+		System.out.println();
+		star--;
+		
+	}
+}
+}
